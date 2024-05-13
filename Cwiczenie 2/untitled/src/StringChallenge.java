04b175ad405708eceaeceecd6c4b54ee0cd639c3
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2024-05-13 12:17:30 +0200

    13.05.2024

---
+++ Cwiczenie 2/untitled/src/StringChallenge.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StringChallenge {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Podaj swoje imie");
+        String name = reader.readLine();
+
+        System.out.println("POdaj swoje miasto");
+        String city = reader.readLine();
+
+        System.out.println("Twoje imie to: " + name+". Mieszkasz w "+city);
+        System.out.println("Liczba liter w miescie to :"+city.length());
+
+
+
+
+    }
+
+}
