336fcddab0dc3ef59e591aa198a5d74a83b31873
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2024-05-21 11:59:39 +0200

    21.05.2024

--- Cwiczenie 5/instrukcjie warunkowe/src/Main.java
+++ Cwiczenie 5/instrukcjie warunkowe/src/Main.java
@@ -15,5 +15,40 @@ public class Main {
         int a = 10;
         int b = a > 5 ? 3 : 99;
 
+        // Switch
+        int num = 9;
+
+        switch (num) {
+            case 3:
+            case 4:
+                System.out.println("nie");
+                break;
+            case 7:
+                System.out.println("tak");
+                break;
+            default:
+                System.out.println("niewiem");
+        }
+
+        // For
+        String namesFor[] = {"Mikołaj", "Mikołaj1"};
+        for (int i = 0; i < namesFor.length; i++) {
+            // for (String s : namesFor) {  to samo to wyzej, petla for each
+            System.out.println(namesFor[i]);
+            // break;  ---> przerywa całą petle
+            // continue;  ---> przerywa aktualnie wykonywaną iteracje
+        }
+
+        // While
+        while (num >= 0) {
+            System.out.println(num);
+            num--;
+        }
+
+        // For each
+        for (String s : namesFor) {
+            System.out.println(s);
+        }
+
     }
 }
