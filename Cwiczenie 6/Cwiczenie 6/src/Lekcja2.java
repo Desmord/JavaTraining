a4a23257f701e7378898192ae33ce3e386d4fc11
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2024-05-26 17:32:00 +0200

    26.05.2024

--- Cwiczenie 6/Cwiczenie 6/src/Lekcja2.java
+++ Cwiczenie 6/Cwiczenie 6/src/Lekcja2.java
@@ -3,32 +3,51 @@ class Employee {
     String name;
     String surName;
     String jobTitle;
+    int earnings;
 
     // Można korzystać z dwoch konstruktorów
-    public Employee(){
+    public Employee() {
         this.name = "not defined";
         this.surName = "not defined";
         this.jobTitle = "not defined";
+        this.earnings = 1000;
     }
 
-    public Employee(String name, String surName, String jobTitle) {
+    public Employee(String name, String surName, String jobTitle,int earnings) {
         this.name = name;
         this.surName = surName;
         this.jobTitle = jobTitle;
+        this.earnings = earnings;
     }
 
     public void printData() {
         System.out.println("Imie: " + this.name);
         System.out.println("Nazwisko: " + this.surName);
         System.out.println("Nazwa stanowiska: " + this.jobTitle);
+        System.out.println("Obecne zarobki: " + earnings);
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void addEarnings(int pay) {
+        this.earnings += pay;
+    }
+
+    public void printEarnings() {
+        System.out.println("Obecne zarobki: " + earnings);
     }
 }
 
 public class Lekcja2 {
     public static void main(String[] args) {
 
-        Employee pracownik = new Employee("Mikołaj", "Chojnacki", "Doradca klienta");
+        Employee pracownik = new Employee("Mikołaj", "Chojnacki", "Doradca klienta",1000);
 
         pracownik.printData();
+        pracownik.setName("Nowy Mikołaj");
+        pracownik.addEarnings(100);
+        pracownik.printEarnings();
     }
 }
